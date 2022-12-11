512be68d4abccbea10bc183782cc151562f774b7
sjhlko <sjhlko@naver.com>
2022-12-12 06:33:10 +0900

    ✨ 회원가입 api 추가

--- src/main/java/com/itstime/allpasstival/controller/UserController.java
+++ src/main/java/com/itstime/allpasstival/controller/UserController.java
@@ -1,35 +1,39 @@
 package com.itstime.allpasstival.controller;
 
-import com.itstime.allpasstival.domain.dto.UserResponse;
+import com.itstime.allpasstival.domain.dto.*;
 import com.itstime.allpasstival.domain.entity.User;
 import com.itstime.allpasstival.repository.UserRepository;
+import com.itstime.allpasstival.service.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@Controller
-@RequestMapping("/users")
+@RestController
+@RequestMapping("api/users")
+@RequiredArgsConstructor
 @Slf4j
 public class UserController {
     private final UserRepository userRepository;
-
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final UserService userService;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<UserResponse> userInfo(@PathVariable Integer id){
+    public ResponseEntity<UserInfoResponse> userInfo(@PathVariable Integer id){
         Optional<User> optionalUser = userRepository.findById(id);
-        UserResponse userResponse = UserResponse.builder()
-                .id(optionalUser.get().getUserId())
+        UserInfoResponse userInfoResponse = UserInfoResponse.builder()
+                .email(optionalUser.get().getEmail())
                 .nickname(optionalUser.get().getNickname())
                 .profilPicUrl(optionalUser.get().getProfilPicUrl())
                 .build();
-        return ResponseEntity.ok().body(userResponse);
+        return ResponseEntity.ok().body(userInfoResponse);
     }
+
+    @PostMapping(value = "/register")
+    public Response<UserJoinResponse> register(@RequestBody UserJoinRequest userJoinRequest){
+        UserDto userDto = userService.register(userJoinRequest);
+        return Response.success(new UserJoinResponse(userDto.getEmail(), userDto.getNickname()));
+    }
+
 }
