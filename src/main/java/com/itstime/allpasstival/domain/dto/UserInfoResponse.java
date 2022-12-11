67e5b7c4d8734d4459b235b4860ae87754df9d38
sjhlko <sjhlko@naver.com>
2022-12-12 06:37:02 +0900

    🚚 UserResponse.java 를 UserInfoResponse.java로 이름 변경

--- src/main/java/com/itstime/allpasstival/domain/dto/UserInfoResponse.java
+++ src/main/java/com/itstime/allpasstival/domain/dto/UserInfoResponse.java
@@ -1,13 +1,14 @@
 package com.itstime.allpasstival.domain.dto;
 
+import com.itstime.allpasstival.domain.entity.User;
 import lombok.*;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserResponse {
-    private Integer id;
+public class UserInfoResponse {
+    private String email;
     private String nickname;
     private String profilPicUrl;
 
