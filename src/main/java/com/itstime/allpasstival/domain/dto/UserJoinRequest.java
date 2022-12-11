c90926f7b52c51652497c425d8a4823e9f127ba6
sjhlko <sjhlko@naver.com>
2022-12-12 06:31:37 +0900

    ✨ UserJoin 관련 Dto 추가

---
+++ src/main/java/com/itstime/allpasstival/domain/dto/UserJoinRequest.java
@@ -0,0 +1,25 @@
+package com.itstime.allpasstival.domain.dto;
+
+import com.itstime.allpasstival.domain.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserJoinRequest {
+    private String email;
+    private String password;
+    private String nickname;
+
+    public User toEntity(String hashedPassword){
+        return User.builder()
+                .email(this.email)
+                .password(hashedPassword)
+                .nickname(this.nickname)
+                .build();
+    }
+}
