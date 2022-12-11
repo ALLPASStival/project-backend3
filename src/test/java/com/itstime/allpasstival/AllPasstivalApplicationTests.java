7a9445cbc10322fa2ccfcaf97ce33a7bc6981cf6
sjhlko <sjhlko@naver.com>
2022-12-11 16:15:40 +0900

    🎉 프로젝트 초기 설정

---
+++ src/test/java/com/itstime/allpasstival/AllPasstivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itstime.allpasstival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllPasstivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
