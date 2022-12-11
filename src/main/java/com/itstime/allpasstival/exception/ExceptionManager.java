6a669e103ac333e8ead1848778e458cb29dadcf1
sjhlko <sjhlko@naver.com>
2022-12-12 06:32:29 +0900

    ✨ 에러처리 관련 메소드 추가

---
+++ src/main/java/com/itstime/allpasstival/exception/ExceptionManager.java
@@ -0,0 +1,24 @@
+package com.itstime.allpasstival.exception;
+
+import com.itstime.allpasstival.domain.dto.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionManager {
+    @ExceptionHandler(AllPasstivalAppException.class)
+    public ResponseEntity<?> hospitalReviewAppExceptionHandler(AllPasstivalAppException e){
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(Response.error(e.getErrorCode().name(), e.getMessage()));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> runtimeExceptionHandler(RuntimeException e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Response.error("",e.getMessage()));
+    }
+
+
+}
