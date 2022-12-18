71a02f729328ad9f690fbdabb30055474c41b16e
sjhlko <sjhlko@naver.com>
2022-12-18 19:26:18 +0900

    ✏️ 배포를 위한 @SpringBootTest 주석처리

--- src/test/java/com/itstime/allpasstival/AllPasstivalApplicationTests.java
+++ src/test/java/com/itstime/allpasstival/AllPasstivalApplicationTests.java
@@ -3,7 +3,7 @@ package com.itstime.allpasstival;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AllPasstivalApplicationTests {
 
 	@Test
