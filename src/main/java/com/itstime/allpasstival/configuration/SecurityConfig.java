f3a3c47d9e506b99f3cb2457d756e73a0272c10a
sjhlko <sjhlko@naver.com>
2022-12-30 15:07:02 +0900

    ✨ 유저 로그인 예외처리 추가

--- src/main/java/com/itstime/allpasstival/configuration/SecurityConfig.java
+++ src/main/java/com/itstime/allpasstival/configuration/SecurityConfig.java
@@ -17,7 +17,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Value("${jwt.token.secret}")
+    private String secretKey;
+
+    private final UserService userService;
 
     private String[] SWAGGER = {
             /* swagger v2 */
@@ -49,7 +55,10 @@ public class SecurityConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt사용하는 경우 씀
                 .and()
-                //.addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class) //UserNamePasswordAuthenticationFilter적용하기 전에 JWTTokenFilter를 적용 하라는 뜻
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and()
+                .addFilterBefore(new JwtTokenFilter(secretKey,userService), UsernamePasswordAuthenticationFilter.class) //UserNamePasswordAuthenticationFilter적용하기 전에 JWTTokenFilter를 적용 하라는 뜻
                 .build();
     }
 }
--- src/main/java/com/itstime/allpasstival/exception/ErrorCode.java
+++ src/main/java/com/itstime/allpasstival/exception/ErrorCode.java
@@ -10,7 +10,10 @@ public enum ErrorCode {
     DUPLICATED_USER_EMAIL(HttpStatus.CONFLICT,"User email duplicated"),
     DUPLICATED_USER_NICKNAME(HttpStatus.CONFLICT,"User nickname duplicated"),
     NOT_FOUND(HttpStatus.NOT_FOUND, "user doesn't exist"),
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "password is not matched")
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "password is not matched"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token is not valid."),
+    INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "User authorization failed"),
+    DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "database error")
     ;
     private HttpStatus status;
     private String message;
