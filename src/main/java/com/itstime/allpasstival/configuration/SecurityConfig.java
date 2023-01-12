ce46ec6cac460145544d2ce8725b8af482e55a2f
sjhlko <sjhlko@naver.com>
2023-01-12 17:51:34 +0900

    ✨ Post 상세정보 조회 기능 추가

--- src/main/java/com/itstime/allpasstival/configuration/SecurityConfig.java
+++ src/main/java/com/itstime/allpasstival/configuration/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                 .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
                 .antMatchers(HttpMethod.PUT, "/api/v1/**").authenticated()
                 .antMatchers(HttpMethod.DELETE, "/api/v1/**").authenticated()
-                .antMatchers(HttpMethod.GET, "/api/v1/**").authenticated()
+//                .antMatchers(HttpMethod.GET, "/api/v1/**").authenticated()
                 .antMatchers(HttpMethod.PATCH, "/api/v1/**").authenticated()
                 .and()
                 .sessionManagement()
